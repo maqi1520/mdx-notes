9f7ab8464baf0c1ac205c12b97df94336b6f0a6d
马琪斌 <maqi1520@163.com>
2022-04-05 23:32:38 +0800

    fix：eslint

--- src/components/MDX/MDXComponents.jsx
+++ src/components/MDX/MDXComponents.jsx
@@ -4,32 +4,7 @@
 import React from 'react'
 
 import { H1, H2, H3, H4 } from './Heading'
-import InlineCode from './InlineCode'
 import CodeBlock from './CodeBlock'
-
-const P = (p) => <p className="whitespace-pre-wrap my-4" {...p} />
-
-const Link = (p) => <a className="text-primary-500" {...p} />
-
-const Strong = (strong) => <strong className="font-bold" {...strong} />
-
-const OL = (p) => <ol className="ml-6 my-3 list-decimal" {...p} />
-const LI = (p) => <li className="leading-relaxed mb-1" {...p} />
-const UL = (p) => <ul className="ml-6 my-3 list-disc" {...p} />
-
-const Divider = () => (
-  <hr className="my-6 block border-b border-border dark:border-border-dark" />
-)
-
-const Blockquote = ({ children, ...props }) => {
-  return (
-    <>
-      <blockquote className="border-l-4 pl-4 py-1 bg-slate-50" {...props}>
-        <span className="block relative">{children}</span>
-      </blockquote>
-    </>
-  )
-}
 
 export const MDXComponents = {
   wrapper: (props) => <section className="markdown-body" {...props} />,
