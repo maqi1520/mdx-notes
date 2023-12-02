7a89ab1946e415ba8a740efc8c6fdfd2680347ff
马琪斌 <maqi1520@163.com>
2023-12-02 20:13:13 +0800

    feat: 🎸 add tauri_plugin_window_state plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,13 +5,29 @@ mod menu;
 
 use menu::{get_menu, menu_event_handle};
 
+use tauri_plugin_window_state::Builder as windowStatePlugin;
+
 fn main() {
     let menu = get_menu();
 
     tauri::Builder::default()
+        .plugin(windowStatePlugin::default().build())
         .menu(menu)
         .on_menu_event(menu_event_handle)
         .invoke_handler(tauri::generate_handler![folder::show_in_folder])
+        .on_window_event(|event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
+                #[cfg(target_os = "macos")]
+                {
+                    event.window().minimize().unwrap();
+                }
+
+                #[cfg(not(target_os = "macos"))]
+                event.window().close().unwrap();
+
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
