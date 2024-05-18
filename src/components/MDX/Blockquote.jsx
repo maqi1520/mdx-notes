61ec5def619d031c8fc05f073fd832b6fd7db464
马琪斌 <maqi1520@163.com>
2024-05-18 22:29:15 +0800

    fix: 🐛 callout 样式

--- src/components/MDX/Blockquote.jsx
+++ src/components/MDX/Blockquote.jsx
@@ -17,10 +17,10 @@ const iconMap = {
 
 export default function Blockquote({ children, ...other }) {
   const text = children[1]?.props.children || ''
-  const calloutRegex = /\[!(\w+)\](?:\s*(.*))?\n([\s\S]*?)(?=\n\[!|\n?$)/g
+  const calloutRegex = /\[!(\w+)\](?:\s*(.*))?(\n([\s\S]*?)(?=\n\[!|\n?$))?/
   const match = calloutRegex.exec(text)
   if (match !== null) {
-    const [fullMatch, type, title, content] = match
+    const [fullMatch, type, title, nextLine = '', content = ''] = match
     const t = type.toLocaleLowerCase()
     const texts = content.split('\n')
 
@@ -31,11 +31,13 @@ export default function Blockquote({ children, ...other }) {
             {iconMap[t] || <PencilIcon />} {title}
           </section>
         )}
-        <section className="callout-content">
-          {texts.map((p, index) => (
-            <section key={index}>{p}</section>
-          ))}
-        </section>
+        {content && (
+          <section className="callout-content">
+            {texts.map((p, index) => (
+              <section key={index}>{p}</section>
+            ))}
+          </section>
+        )}
       </section>
     )
   }
@@ -43,9 +45,11 @@ export default function Blockquote({ children, ...other }) {
 
   return (
     <blockquote {...other}>
-      {texts.map((p, index) => (
-        <p key={index}>{p}</p>
-      ))}
+      {text === '' ? (
+        <p style={{ overflow: 'hidden' }}></p>
+      ) : (
+        texts.map((p, index) => <p key={index}>{p}</p>)
+      )}
     </blockquote>
   )
 }
